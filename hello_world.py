804eba676a4656dbee3ffa593c1f8835ebbda5ce
ArtyomAbesadze1 <aabesadze.ieu2021@student.ie.edu>
2023-02-01 18:41:03 +0100

    This is a commit

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+
+
+choice = input("Choose A or B: ")
+if choice == "A":
+   print("You chose A")
+if choice == "B":
+   print("you chose B")
+elif choice != "A" or "B":
+   print("You didn't choose")
+
+
+
+
+#fruits = ["apple", "banana", "cherry"]
+#for x  in fruits:
+# print("I like",(fruits))
+
